1ab891c07167c8b7aebca3b89f8bea74cac90281
AmaalRaj <rajamaltr@gmail.com>
2021-03-03 15:25:41 +0530

    function sample bash script

---
+++ functions.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+# function fun()
+# {
+#     echo "Hey Function is here"
+# }
+
+# fun
+
+###################
+
+##Passing arguments
+
+function fun()
+{
+    echo "Hey Function is here $1 $2"
+}
+
+fun its me
